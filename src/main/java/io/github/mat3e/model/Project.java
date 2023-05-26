6e2766bbaa4c3d707341c8302e565bda3194740a
KozlowskiKamil <kamil2k8@gmail.com>
2023-05-26 11:25:53 +0200

    Działający

--- src/main/java/io/github/mat3e/model/Project.java
+++ src/main/java/io/github/mat3e/model/Project.java
@@ -1,12 +1,6 @@
 package io.github.mat3e.model;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Set;
 
