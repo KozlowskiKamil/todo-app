ae4dbde4a481eca5ce8164641e1c30c5d9c68166
KozlowskiKamil <kamil2k8@gmail.com>
2023-05-26 10:46:00 +0200

    ?

--- src/main/java/io/github/mat3e/TodoAppApplication.java
+++ src/main/java/io/github/mat3e/TodoAppApplication.java
@@ -2,8 +2,6 @@ package io.github.mat3e;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
