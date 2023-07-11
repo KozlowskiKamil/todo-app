58bd2b897df41fdfd2a2f8a08c2eb1b37061dbe8
KozlowskiKamil <kamil2k8@gmail.com>
2023-07-11 14:39:03 +0200

    add 2 integration test

---
+++ src/test/java/io/github/mat3e/controller/TestControllerLightIntegrationTest.java
@@ -0,0 +1,50 @@
+package io.github.mat3e.controller;
+
+import io.github.mat3e.model.Task;
+import io.github.mat3e.model.TaskRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultHandler;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TaskController.class)
+@ActiveProfiles("integration")
+public class TestControllerLightIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TaskRepository repo;
+
+    @Test
+    void httpGet_returnsGivenTask() throws Exception {
+        // givern
+        String description = "foo";
+        when(repo.findById(anyInt()))
+                .thenReturn(Optional.ofNullable(new Task(description, LocalDateTime.now())));
+        //when
+        mockMvc.perform(get("/tasks/" + 12))
+                .andDo(print())
+                .andExpect(content().string(containsString(description)));
+    }
+
+}
