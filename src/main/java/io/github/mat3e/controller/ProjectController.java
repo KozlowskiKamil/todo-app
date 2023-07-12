65498bdc0963e096d3f045b1c0006643bfeeca8a
KozlowskiKamil <kamil2k8@gmail.com>
2023-07-12 03:13:48 +0200

    add thymeleaf

--- src/main/java/io/github/mat3e/controller/ProjectController.java
+++ src/main/java/io/github/mat3e/controller/ProjectController.java
@@ -1,9 +1,12 @@
 package io.github.mat3e.controller;
 
+import io.github.mat3e.model.ProjectStep;
 import io.github.mat3e.model.projection.ProjectWriteModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,5 +17,11 @@ public class ProjectController {
     String showProjects(Model model) {
         model.addAttribute("project", new ProjectWriteModel());
         return "projects";
+    }
+
+    @PostMapping(params = "addStep")
+    String addProjectStep(@ModelAttribute("project") ProjectWriteModel current) {
+        current.getSteps().add(new ProjectStep());
+        return "project";
     }
 }
